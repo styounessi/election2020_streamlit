1ee46cb17416d0980ec4d330c3f34ada8248c147
Steve Younessi <63491724+styounessi@users.noreply.github.com>
2022-10-24 09:28:38 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import sqlite3
+from PIL import Image
+
+st.set_page_config(layout='wide')
+
+cnxn = sqlite3.connect('election_2020.db')
+
+@st.cache(allow_output_mutation=True)
+def query_to_dataframe():
+    query = pd.read_sql('SELECT * FROM state_averages ORDER BY state', cnxn)
+    return query
+
+averages = query_to_dataframe().copy()
+averages['modeldate'] = pd.to_datetime(averages['modeldate'])
+
+st.title('2020 Presidential Election Polling Averages')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    img1 = Image.open('imgs/i_voted1.jpg')
+    st.image(img1)
+
+with col2:
+    img2 = Image.open('imgs/i_voted2.jpg')
+    st.image(img2)
+
+st.header('Time series visualization of polling averages from the 2020 Presidential Election via 538')
+
+state_list = averages['state'].unique()
+state = st.selectbox('Select a State:', state_list)
+
+fig = px.line(averages[averages['state'] == state],
+              x='modeldate',
+              y='pct_estimate',
+              color='candidate_name',
+              height=800)
+
+fig.update_layout(xaxis_title='Date',
+                  yaxis_title='Percent Estimated',
+                  legend_title='Candidate')
+
+st.plotly_chart(fig, use_container_width=True)
---
+++ preprocess.py
@@ -0,0 +1,26 @@
+import pandas as pd 
+import sqlite3
+
+poll_avg = pd.read_csv('https://raw.githubusercontent.com/bconnor17/2020-Presidential-Election-Data/master/presidential_poll_averages_2020.csv')
+
+poll_avg.drop('cycle', axis=1, inplace=True)
+poll_avg = poll_avg[poll_avg['candidate_name'].str.contains('Convention Bounce for Joseph R. Biden Jr.') == False]
+poll_avg = poll_avg[poll_avg['candidate_name'].str.contains('Convention Bounce for Donald Trump') == False]
+
+cnxn = sqlite3.connect('election_2020.db')
+c = cnxn.cursor()
+
+c.execute('''
+          CREATE TABLE state_averages (
+          id INTEGER PRIMARY KEY,
+          state TEXT NOT NULL,
+          modeldate TEXT NOT NULL,
+          candidate_name TEXT NOT NULL,
+          pct_estimate REAL NOT NULL,
+          pct_trend_adjusted REAL NOT NULL
+          )
+          ''')
+
+poll_avg.to_sql('state_averages', cnxn, if_exists='append', index=False)
+
+cnxn.close()
